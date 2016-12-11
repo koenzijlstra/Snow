81130b6651eec9b06f38b31d340a9c080d6bd757
koenzijlstra <koen.zijlstra@student.uva.nl>
2016-12-11 19:41:06 +0100

    Most likely finished google sign-in! after signing in, go to search activity

--- app/src/main/java/com/example/koen/snow/Sign_in_Activity.java
+++ app/src/main/java/com/example/koen/snow/Sign_in_Activity.java
@@ -1,19 +1,24 @@
 package com.example.koen.snow;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class Sign_in_Activity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener  {
 
@@ -48,7 +53,7 @@ public class Sign_in_Activity extends AppCompatActivity implements GoogleApiClie
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.sign_in_button:
-                sigIn();
+                signIn();
                 break;
             case R.id.signOutButton:
                 signOut();
@@ -76,8 +81,31 @@ public class Sign_in_Activity extends AppCompatActivity implements GoogleApiClie
         Log.d(TAG, "handleSignInResult:" + result.isSuccess());
         if (result.isSuccess()){
             GoogleSignInAccount acct = result.getSignInAccount();
-            statusTextView.setText("Hello, " + acct.getDisplayName());
+            assert acct != null;
+            Toast.makeText(this, "Welcome "+ acct.getDisplayName(), Toast.LENGTH_LONG).show();
+
+            Intent gotosearch = new Intent(this, MainActivity.class);
+            startActivity(gotosearch);
         } else {
         }
     }
+
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult){
+        Log.d(TAG, "onConnectionFailed" + connectionResult);
+    }
+
+    private void signOut(){
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                statusTextView.setText("signed out");
+            }
+        });
+    }
+
+
+
+
 }
